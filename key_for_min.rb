ea06a963a3e31e1072a6f04ed4407ed48b79911b
Harley McDowell <hmcdowell95@gmail.com>
2020-01-31 23:03:11 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,19 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
 
+def key_for_min_value(name_hash)
+  if name_hash == {}
+    return nil
+  else
+    min_value = name_hash.first[1]
+    min_key = name_hash.first[0]
+    name_hash.each do |k, v|
+      if v < min_value
+        min_value = v 
+        min_key = k 
+      end
+    end 
+    min_key
+  end
 end
