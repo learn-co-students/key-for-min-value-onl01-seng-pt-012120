5ab7f0ab49ad9e63972a3013d30cde793bc9f2cf
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-02-04 00:21:50 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,17 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+# def key_for_min_value(name_hash)
+# name_hash = {:"Joe" => 2, :"Ashley" => 5, :"Mary" => 4}
+# puts "Joe"
+# end
 
-def key_for_min_value(name_hash)
 
+def key_for_min_value(hash)
+  lowest_key = nil
+  lowest_value = Float::INFINITY
+  hash.each do |k, v|
+    if v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
+  end
+  lowest_key
 end
